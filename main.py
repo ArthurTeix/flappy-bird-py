5850502e466346d57d73c0e495b66251d57b7f27
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-08-25 23:30:49 -0300

    feat: init screen and elements

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+import os 
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 800
+
+IMG_PIPE = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMG_GROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'ground.png')))
+IMG_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMGS_BIRD = [
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+]
