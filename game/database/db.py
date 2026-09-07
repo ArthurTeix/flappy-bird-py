80e341ed85586b4ec19d3104cc074a8d51875a8c
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-09-07 17:51:56 -0300

    feat: create top 5 scores

--- game/database/db.py
+++ game/database/db.py
@@ -24,3 +24,18 @@ def save_score(score):
 
     connection.commit()
     connection.close()
+
+
+def get_top5_score():
+    connection = sqlite3.connect('./game/database/rank.db')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT score FROM matches ORDER BY score DESC LIMIT 5")
+    top5 = cursor.fetchall()
+
+    rank = []
+    for t in top5:
+        rank.append(t[0])
+
+    connection.close()
+    return rank
